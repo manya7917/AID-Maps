8c35925b09b6ce0171319b69405446fba9782243
Niharika <105709579+nemo-07@users.noreply.github.com>
2023-04-18 22:18:27 +0530

    Update otp.py

--- AID MAPS FINAL/otp.py
+++ AID MAPS FINAL/otp.py
@@ -7,10 +7,10 @@ def generateOTP(otp_size = 6):
         final_otp = final_otp + str(random.randint(0,9))
     return final_otp
 
-def sendEmailVerificationRequest(sender="indiannk2002@gmail.com",receiver="yshivam9920@gmail.com", custom_text="Hello, Your OTP is "):
+def sendEmailVerificationRequest(sender="sender@gmail.com",receiver="reciever9920@gmail.com", custom_text="Hello, Your OTP is "):
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
-    google_app_password = "hlagmnfpjluwpoes"
+    google_app_password = "password"
     server.login(sender,google_app_password)
     cur_otp = generateOTP()
     msg = custom_text +  cur_otp
